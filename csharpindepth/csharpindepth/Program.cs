8f80d64e3a434f71649535793de55aa7fbf601d3
pezy <urbancpz@gmail.com>
2017-09-08 17:24:10 +0800

    use lambda and extension method.

--- csharpindepth/csharpindepth/Program.cs
+++ csharpindepth/csharpindepth/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
 
 namespace csharpindepth
 {
@@ -10,7 +12,7 @@ namespace csharpindepth
     {
         static void Main(string[] args)
         {
-            foreach (var p in Product.SampleProducts)
+            foreach (var p in Product.SampleProducts.Where(p => p.Price > 10).OrderBy(p => p.Name)) // extension method.
             {
                 Console.WriteLine(p.ToString());
             }
