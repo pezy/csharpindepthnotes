a0ddb19842c78ff257a9c3eed051882d38195fba
pezy <urbancpz@gmail.com>
2017-09-11 10:23:09 +0800

    try to write to Excel.

--- csharpindepth/csharpindepth/Program.cs
+++ csharpindepth/csharpindepth/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Forms;
+using Microsoft.Office.Interop.Excel;
 
 namespace csharpindepth
 {
@@ -16,6 +16,19 @@ namespace csharpindepth
             {
                 Console.WriteLine(p.ToString());
             }
+
+            var app = new Application { Visible = false };
+            Workbook workbook = app.Workbooks.Add();
+            Worksheet worksheet = app.ActiveSheet;
+            int row = 1;
+            foreach (var product in Product.SampleProducts.Where(p => p.Price > 10))
+            {
+                worksheet.Cells[row, 1].Value = product.Name;
+                worksheet.Cells[row, 2].Value = product.Price;
+                row++;
+            }
+            workbook.SaveAs(Filename: "demo.xls", FileFormat: XlFileFormat.xlWorkbookNormal);
+            app.Application.Quit();
         }
     }
 }
